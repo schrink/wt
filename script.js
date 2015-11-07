dac522e06b52251eb875134bf3b3d6104a5d51c5
Your Name <you@example.com>
2015-11-07 11:22:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$.ajax({
+	url: 'http://api.openweathermap.org/data/2.5/weather?q=Belgrade,Serbia&appid=2de143494c0b295cca9337e1e96b00e0&units=metric',
+	method: 'get',
+	success: function(data){
+
+		$('#temp span').text(data.main.temp)
+	
+	}
+
+});
