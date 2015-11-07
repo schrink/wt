08495a5fe3cb722bfa48690e1b803a02b9c94055
Your Name <you@example.com>
2015-11-07 11:30:53 +0100

    fix

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-$.ajax({
+
+
+$('button').on('click', function(){
+	$.ajax({
 	url: 'http://api.openweathermap.org/data/2.5/weather?q=Belgrade,Serbia&appid=2de143494c0b295cca9337e1e96b00e0&units=metric',
 	method: 'get',
 	success: function(data){
@@ -8,3 +11,5 @@ $.ajax({
 	}
 
 });
+
+})
